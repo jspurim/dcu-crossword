854233e8a9aea56d27d0f9e92f6d4f1004fb215e
Cirano Eusebi <eusebi.cirano@gmail.com>
2023-09-30 19:07:20 -0300

    (fix) Allow shift-tab to go back in the grid. Make it so backspace doesn't jump back a field when it had a letter to remove.

--- src/app/auto-tab-on-full.directive.ts
+++ src/app/auto-tab-on-full.directive.ts
@@ -4,52 +4,65 @@ import { Directive, HostListener } from '@angular/core';
   selector: 'input[autoTabOnFull]'
 })
 export class AutoTabOnFullDirective {
-  @HostListener('keyup', ['$event']) onKeyDown(keyboardEvent: KeyboardEvent) {
+  // Use keydown event to be able to know if we had a letter before it gets
+  // removed by the backspace.
+  @HostListener('keydown.Backspace', ['$event']) onBackspaceDown(keyboardEvent: KeyboardEvent) {
     const target = keyboardEvent.target as
       | HTMLInputElement
       | HTMLTextAreaElement
       | null;
 
-    if(target == null) return;
-    if (target.value.length == 0 && keyboardEvent.key == "Backspace") {
+    // This check is for typing inference. We would never get a keyboard event
+    // from anything different than HTMLInputElement or HTMLTextAreaElement
+    if (target == null) return;
+    // Only move to the previous field if we had started with an empty field.
+    if (target.value.length == 0) {
+      this.findPreviousTarget(target)?.focus();
       keyboardEvent.preventDefault();
-
-      const { type } = target;
-      let current = target.parentElement?.previousElementSibling?.firstElementChild
-  
-      while (current) {
-        if (
-          (current as HTMLInputElement | HTMLTextAreaElement).type ===
-          type
-        ) {
-          (current as HTMLInputElement | HTMLTextAreaElement).focus();
-          return;
-        }
-  
-        current = current.parentElement?.previousElementSibling?.firstElementChild
-      }
     }
-
-    if (target.maxLength == target.value.length) {
-      keyboardEvent.preventDefault();
-
-      const { type } = target;
-      let current = target.parentElement?.nextElementSibling?.firstElementChild
-  
-      while (current) {
-        if (
-          (current as HTMLInputElement | HTMLTextAreaElement).type ===
-          type
-        ) {
-          (current as HTMLInputElement | HTMLTextAreaElement).focus();
-          return;
-        }
-  
-        current = current.parentElement?.nextElementSibling?.firstElementChild
-      }
-    }
-
-
   }
 
+  @HostListener('keyup', ['$event']) onKeyUp(keyboardEvent: KeyboardEvent) {
+    const target = keyboardEvent.target as
+      | HTMLInputElement
+      | HTMLTextAreaElement
+      | null;
+
+    // This check is for typing inference. We would never get a keyboard event
+    // from anything different than HTMLInputElement or HTMLTextAreaElement
+    if (target == null) return;
+    // Only process events for visible characters (so it doesn't trigger when tabbing or pressing shift/alt/insert as they could interfere with narrator controls)
+    const allowedPattern = /^[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]$/;
+    if (!allowedPattern.test(keyboardEvent.key)) return;
+
+    // This code is reached when the key is already added to the input field triggered.
+    // So we should only check for the field being full
+    if (target.value.length === target.maxLength) {
+      this.findNextTarget(target)?.focus();
+    }
+  }
+
+  findPreviousTarget(initialElement: HTMLInputElement | HTMLTextAreaElement): HTMLInputElement | HTMLTextAreaElement | null {
+    const { type } = initialElement;
+    let current = initialElement.parentElement?.previousElementSibling?.firstElementChild
+
+    while (current) {
+      let castedCurrent = (current as HTMLInputElement | HTMLTextAreaElement);
+      if (castedCurrent.type === type) return castedCurrent;
+      current = current.parentElement?.previousElementSibling?.firstElementChild
+    }
+    return null;
+  }
+
+  findNextTarget(initialElement: HTMLInputElement | HTMLTextAreaElement): HTMLInputElement | HTMLTextAreaElement | null {
+    const { type } = initialElement;
+    let current = initialElement.parentElement?.nextElementSibling?.firstElementChild
+
+    while (current) {
+      let castedCurrent = (current as HTMLInputElement | HTMLTextAreaElement);
+      if (castedCurrent.type === type) return castedCurrent;
+      current = current.parentElement?.nextElementSibling?.firstElementChild
+    }
+    return null;
+  }
 }
